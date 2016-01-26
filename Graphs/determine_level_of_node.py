94475fe1d741354c0cd944bbb2eb1ce0fe6e2b85
Chakshu Ahuja <ahuja.chaks@gmail.com>
2016-01-26 20:45:49 +0530

    Level of node in graph

---
+++ Graphs/determine_level_of_node.py
@@ -0,0 +1,53 @@
+no_of_nodes = int(raw_input())
+no_of_edges = int(raw_input())
+
+#Implementing the adjacency list
+adj_list = {}
+for i in range(no_of_nodes):
+    adj_list[i] = []
+
+print adj_list
+for i in range(no_of_edges):
+    x, y = map(int, raw_input().split())
+    adj_list[x].append(y)
+
+#Implementing the Queue and basic operations
+class Queue:
+    def __init__(self):
+        self.items = []
+    def enqueue(self, item):
+        self.items.insert(0, item)
+    def dequeue(self):
+        return self.items.pop()
+    def isEmpty(self):
+        return self.items == []
+    def size(self):
+        return len(self.items)
+
+#Implementing the Graph Traversal BFS
+visited = []
+for i in range(no_of_nodes):
+    visited.append(False)
+
+level = []
+for i in range(no_of_nodes):
+    level.append(-1)
+
+def BFS(G, s):
+    Q = Queue()
+    Q.enqueue(s)
+    level[s] = 0
+    visited[s] = True
+    while Q.size():
+        v = Q.dequeue()
+        print v
+        for w in G[v]:
+            if not visited[w]:
+                level[w] = level[v] + 1
+                Q.enqueue(w)
+                visited[w] = True
+
+
+BFS(adj_list, 0)
+print level
+    
